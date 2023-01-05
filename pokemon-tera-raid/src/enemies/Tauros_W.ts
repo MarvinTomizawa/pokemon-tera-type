0fef4408e80339c03b54cc86a41447700b1d840c
CloudGriffith <37061423+CloudGriffith@users.noreply.github.com>
2023-01-05 13:30:14 -0300

    Correções no move headbutt do Tauros (F) e Tauros (W) adicionado com todos os moves

--- pokemon-tera-raid/src/enemies/Tauros_W.ts
+++ pokemon-tera-raid/src/enemies/Tauros_W.ts
@@ -1,6 +1,8 @@
 import {EType} from "../common/types";
 import {EStatus} from "../common/status";
 import {Pokemon} from "../common/Pokemon.interface";
+import { ECategory } from "../common/cayegory";
+import { EWeather_Terrain } from "../common/Weather_Terrain";
 
 export const TaurosW: Pokemon = {
     name: "Tauros (W)",
@@ -8,7 +10,35 @@ export const TaurosW: Pokemon = {
         {
             name: 'Wave Crash',
             type: EType.Water,
+            category: ECategory.Attack,
+        },
+        {
+            name: 'Close Combat',
+            type: EType.Fighting,
+            category: ECategory.Attack,
+        },
+        {
+            name: 'Surf',
+            type: EType.Water,
+            category: ECategory.SPAttack,
+        },
+        {
+            name: 'Headbutt',
+            type: EType.Normal,
+            status: EStatus.Flinch,
+            category: ECategory.Attack,
+        },
+        {
+            name: 'Rain Dance',
+            type: EType.Water,
+            terrain: EWeather_Terrain.Rain,
+            category: ECategory.status,
+        },
+        {
+            name: 'Bulk Up',
+            type: EType.Fighting,
+            category: ECategory.Status,
         },
     ],
-    types: [EType.Ghost],
+    types: [EType.Fighting, EType.Water],
 };
