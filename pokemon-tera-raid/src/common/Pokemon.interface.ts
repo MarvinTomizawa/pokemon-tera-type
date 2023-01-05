6d7339166290e688060f5543bb99dc33d25848ba
CloudGriffith <37061423+CloudGriffith@users.noreply.github.com>
2023-01-04 22:50:44 -0300

    Adicionado status no ataque

--- pokemon-tera-raid/src/common/Pokemon.interface.ts
+++ pokemon-tera-raid/src/common/Pokemon.interface.ts
@@ -1,10 +1,12 @@
 import {EType} from "./types";
 import {EStatus} from "./status";
+import { EWeather_Terrain } from "./Weather_Terrain";
 
 export interface PokemonMove {
     name: string;
     type: EType;
     status?: EStatus;
+    terrain?: EWeather_Terrain;
 }
 
 export interface Pokemon {
