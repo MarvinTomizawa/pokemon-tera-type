33c3942800561063d3d4ba32c39a040c59181594
CloudGriffith <37061423+CloudGriffith@users.noreply.github.com>
2023-01-04 22:51:39 -0300

    Tauros de fogo adicionado

---
+++ pokemon-tera-raid/src/enemies/Tauros_F.ts
@@ -0,0 +1,38 @@
+import {EType} from "../common/types";
+import {EStatus} from "../common/status";
+import {Pokemon} from "../common/Pokemon.interface";
+import { EWeather_Terrain } from "../common/Weather_Terrain";
+
+export const TaurosF: Pokemon = {
+    name: "Tauros (F)",
+    mooves: [
+        {
+            name: 'Flare Blitz',
+            type: EType.Fire,
+            status: EStatus.Burn,
+        },
+        {
+            name: 'Close Combat',
+            type: EType.Fighting,
+        },
+        {
+            name: 'Flamethrower',
+            type:  EType.Fire,
+            status: EStatus.Burn,
+        },
+        {
+            name: 'Headbutt',
+            type: EType.Fighting,
+        },
+        {
+            name: 'Sunny Day',
+            type: EType.Fire,
+            terrain: EWeather_Terrain.Sunshine,
+        },
+        {
+            name: 'Bulk Up',
+            type: EType.Fighting,
+        },
+    ],
+    types: [EType.Fighting, EType.Fire],
+};
